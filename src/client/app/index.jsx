8df94541b4b446d2327d47ab45ab721b4f763f13
joeljames <joeljames1985@gmail.com>
2016-12-06 09:35:57 -0800

    Move components to a separate folder

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import LikeComponent from './LikeComponent.jsx';
+import LikeComponent from './components/LikeComponent.jsx';
 
 class App extends React.Component {
   render () {
